50bf5f2a8ac6466af02b6e84cbbe27054043176d
Hamzeh Sayeh <hamzehsayeh6@gmail.com>
2024-09-04 09:18:11 +0300

    otp service

--- main.go
+++ main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
-	"github.com/HamzehSayeh/go-project/handlers"
-	"github.com/go-chi/chi/v5"
-	"github.com/joho/godotenv"
-	"log"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	// Load environment variables
-	err := godotenv.Load()
+
+	url := "https://69ez1r.api.infobip.com/sms/2/text/advanced"
+	method := "POST"
+
+	payload := strings.NewReader(`{"messages":[{"destinations":[{"to":"970597527861"}],"from":"447491163443","text":"Congratulations on sending your first message.\nGo ahead and check the delivery report in the next step."}]}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		fmt.Println(err)
+		return
 	}
+	req.Header.Add("Authorization", "App 51bd59a15eaa6fa6068811fff871dc98-d7913772-54ee-4de1-af79-524f8c11edef")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
 
-	// Create a new router
-	r := chi.NewRouter()
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
 
-	// Define routes
-	r.Post("/send-otp", handlers.SendOTP)
-
-	// Start the server
-	http.ListenAndServe(":8080", r)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
